eeb6f7991abe8ad6a539068d60ab7e3a688bc02e
Yuki xD <pxnditxyr@gmail.com>
2022-05-27 02:18:04 -0400

    fix(manageCodeUrl.ts): Changed delimiter for use '|'

--- src/fast-code/helpers/manageCodeURL.ts
+++ src/fast-code/helpers/manageCodeURL.ts
@@ -1,8 +1,8 @@
 
-export const convertCodeToUrl = ( html : string, css : string, js : string ) => btoa( `${ html }|${ css }|${ js }` );
+export const convertCodeToUrl = ( html : string, css : string, js : string ) => btoa( `${ html }|&*|${ css }|&*|${ js }` );
 export const convertUrlToCode = ( url : string ) => {
     url = url.replace( /\?=/g, '' );
-    const [ htmlUrl, cssUrl, jsUrl ] = atob( url ).split( '|' );
+    const [ htmlUrl, cssUrl, jsUrl ] = atob( url ).split( '|&*|' );
     return { htmlUrl, cssUrl, jsUrl };
 }
 export const changeUrl = ( html : string, css : string, js : string, navigate : ( url : string ) => void ) => {
